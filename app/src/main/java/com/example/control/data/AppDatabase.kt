aac2001969af6eaf28792009ca0cb2dd41a636b8
ALeKayt <vegaalejandro412@gmai.com>
2026-06-23 11:47:12 -0500

    Se actualizo el modelo de la DB,

--- app/src/main/java/com/example/control/data/AppDatabase.kt
+++ app/src/main/java/com/example/control/data/AppDatabase.kt
@@ -34,7 +34,7 @@ import com.example.control.data.entity.UserEntity
     ReviewDetailEntity :: class,
     SaleEntity:: class,
     UserEntity :: class],
-    version = 1,
+    version = 2,
     exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
 
