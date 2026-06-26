a5c8d27d630d4481e742ff242c8ea9a74ed0b118
ALeKayt <vegaalejandro412@gmai.com>
2026-06-26 11:08:27 -0500

    Se arreglaron dependencias y librerias para la correcta ejecucion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,39 +2,39 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.control"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 34  // ← corregido
 
     defaultConfig {
         applicationId = "com.example.control"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = false  // ← sintaxis correcta
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
+        viewBinding = true  // ← unifica los dos bloques en uno
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
@@ -42,11 +42,23 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    //ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // ViewModel para Compose
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    //LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //Fragment
+    implementation(libs.androidx.fragment.ktx)
+    //Activity
+    implementation(libs.androidx.activity.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -54,6 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
 
     //Room
     implementation(libs.androidx.room.runtime)
